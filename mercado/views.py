91cbf148de60a6099205e39b9ab1d814249ad9c5
Dom Henrique <joaohenriqueds24@gmail.com>
2024-12-11 13:59:09 -0300

    Atualizando o app

--- mercado/views.py
+++ mercado/views.py
@@ -28,6 +28,7 @@ def login_view(request):
             return redirect('index')
     return render(request, 'mercado/login.html')
 
+# Views dos arquivos de listagem
 def categoria_list(request):
     categorias = Categoria.objects.all()
     return render(request, 'mercado/categoria_list.html', {'categorias': categorias})
@@ -39,3 +40,51 @@ def cliente_list(request):
 def pedido_list(request):
     pedidos = Pedido.objects.all()
     return render(request, 'mercado/pedido_list.html', {'pedidos': pedidos})
+
+# Views dos arquivos de criação de algum novo item
+def categoria_create(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        Categoria.objects.create(nome=nome)
+        return redirect('categoria_list')
+    return render(request, 'mercado/categoria_create.html')
+
+def cliente_create(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        telefone = request.POST.get('telefone')
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'mercado/cliente_create.html', {
+                'error': 'Nome de usuário já existe.'
+            })
+        # Cria o usuário e associa ao cliente
+        usuario = User.objects.create_user(username=username, password=password)
+        Cliente.objects.create(usuario=usuario, telefone=telefone)
+        return redirect('cliente_list')
+    return render(request, 'mercado/cliente_create.html')
+
+
+def produto_create(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco')
+        categorias = request.POST.getlist('categorias')
+        produto = Produto.objects.create(nome=nome, preco=preco)
+        produto.categorias.set(categorias)
+        return redirect('produto_list') 
+    categorias = Categoria.objects.all()
+    return render(request, 'mercado/produto_create.html', {'categorias': categorias})
+
+def pedido_create(request):
+    if request.method == 'POST':
+        cliente_id = request.POST.get('cliente')
+        produtos_ids = request.POST.getlist('produtos')
+        cliente = Cliente.objects.get(id=cliente_id)
+        pedido = Pedido.objects.create(cliente=cliente)
+        pedido.produtos.set(produtos_ids) 
+        return redirect('pedido_list') 
+    clientes = Cliente.objects.all()
+    produtos = Produto.objects.all()
+    return render(request, 'mercado/pedido_create.html', {'clientes': clientes, 'produtos': produtos})
